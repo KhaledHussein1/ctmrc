6bff7aa170b17d28ae4ffdcfa4e484de4a9c8878
egghead <egghead@example.com>
2025-07-12 20:58:46 -0400

    json

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/admin/:path*",
-        destination: "/admin/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
